8a7d67916fcb497bd54cdcdee15d34e63096219e
Thomas Dietrich <thomas@nurzen.de>
2024-08-31 01:21:24 +0200

    blaa

--- components/wmbus/wmbus.cpp
+++ components/wmbus/wmbus.cpp
@@ -71,10 +71,10 @@ namespace wmbus {
 
       ESP_LOGD(TAG, "Overwriting the telegram, before: %s", telegram.c_str());
       const char* t = "5344a5119955758176078c0028900f002c2577010000968ef8fBe33061Bf7a6700310710a897ec701e67cf0a62f6c55258c1Be7d3825361a07c4224f96f0B6e22Be48Bae5a5ac859524Ba853de0166275df9c626";
-      telegram[0] = "5";
-      telegram[1] = "3";
-      telegram[1] = "3";
-      telegram[1] = "3";
+      telegram[0] = '5';
+      telegram[1] = '3';
+      telegram[2] = '3';
+      telegram[3] = '3';
 
       // ToDo: add manufactures check
       uint32_t meter_id = ((uint32_t)frame[7] << 24) | ((uint32_t)frame[6] << 16) |
